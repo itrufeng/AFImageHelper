ead6fe6829d6c8b8f14e28de95f1ea1fa26653b4
Steve <itrufeng@gmail.com>
2015-10-26 00:09:56 +1100

    Add the ios framework

---
+++ AFImageHelper-IOS/AFImageHelper-IOS.h
@@ -0,0 +1,19 @@
+//
+//  AFImageHelper-IOS.h
+//  AFImageHelper-IOS
+//
+//  Created by Jian Zhang on 10/25/15.
+//  Copyright © 2015 All Forces. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AFImageHelper-IOS.
+FOUNDATION_EXPORT double AFImageHelper_IOSVersionNumber;
+
+//! Project version string for AFImageHelper-IOS.
+FOUNDATION_EXPORT const unsigned char AFImageHelper_IOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AFImageHelper_IOS/PublicHeader.h>
+
+
